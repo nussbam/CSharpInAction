d2484f924703e3a16b6e7afe91ef56a22f1a214f
Robin B <robin.buergi@gmail.com>
2016-11-14 09:15:50 +0100

    Init Testat project

---
+++ Robot/Testat/Form1.cs
@@ -0,0 +1,48 @@
+﻿using RobotCtrl;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Testat
+{
+    public partial class Form1 : Form
+    {
+        Drive drive;
+        public Form1()
+        {
+            InitializeComponent();
+            commonRunParameters.SpeedChanged += new EventHandler(SpeedChanged);
+            commonRunParameters.AccelerationChanged += new EventHandler(AccelerationChanged);
+
+            drive = new Drive();
+            drive.Power = true;
+            runLine.Drive = drive;
+            runTurn.Drive = drive;
+            runArc.Drive = drive;
+
+            SpeedChanged(null, EventArgs.Empty);
+            AccelerationChanged(null, EventArgs.Empty);
+        }
+
+        void AccelerationChanged(object sender, EventArgs e)
+        {
+            runLine.Acceleration = commonRunParameters.Acceleration;
+            runTurn.Acceleration = commonRunParameters.Acceleration;
+            runArc.Acceleration = commonRunParameters.Acceleration;
+        }
+
+        void SpeedChanged(object sender, EventArgs e)
+        {
+            runLine.Speed = commonRunParameters.Speed;
+            runTurn.Speed = commonRunParameters.Speed;
+            runArc.Speed = commonRunParameters.Speed;
+        }
+
+
+    }
+}
