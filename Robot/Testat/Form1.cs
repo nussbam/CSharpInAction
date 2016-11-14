68136f5670c38362dc66142dff2763379542d4d1
Robin B <robin.buergi@gmail.com>
2016-11-14 09:34:20 +0100

    cleanup

--- Robot/Testat/Form1.cs
+++ Robot/Testat/Form1.cs
@@ -1,38 +1,24 @@
-﻿using RobotCtrl;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+﻿using System;
 using System.Windows.Forms;
+using RobotCtrl;
 
 namespace Testat
 {
     public partial class Form1 : Form
     {
-        private Drive drive;
         private RobotConsole rc;
+
         public Form1()
         {
             InitializeComponent();
 
-            // init robot console
-            rc = new RobotConsole();
-            console.RobotConsole = rc;
-            (rc[Switches.Switch1]).SwitchStateChanged += (o, e) => (rc[Leds.Led1]).LedEnabled = (rc[Switches.Switch1]).SwitchEnabled;
-            (rc[Switches.Switch2]).SwitchStateChanged += (o, e) => (rc[Leds.Led2]).LedEnabled = (rc[Switches.Switch2]).SwitchEnabled;
-            (rc[Switches.Switch3]).SwitchStateChanged += (o, e) => (rc[Leds.Led3]).LedEnabled = (rc[Switches.Switch3]).SwitchEnabled;
-            (rc[Switches.Switch4]).SwitchStateChanged += (o, e) => (rc[Leds.Led4]).LedEnabled = (rc[Switches.Switch4]).SwitchEnabled;
-
+            InitConsole();
 
             // init drive
-            commonRunParameters.SpeedChanged += new EventHandler(SpeedChanged);
-            commonRunParameters.AccelerationChanged += new EventHandler(AccelerationChanged);
+            commonRunParameters.SpeedChanged += SpeedChanged;
+            commonRunParameters.AccelerationChanged += AccelerationChanged;
 
-            drive = new Drive();
-            drive.Power = true;
+            var drive = new Drive {Power = true};
             runLine.Drive = drive;
             runTurn.Drive = drive;
             runArc.Drive = drive;
@@ -41,18 +27,32 @@ namespace Testat
             AccelerationChanged(null, EventArgs.Empty);
         }
 
-        void AccelerationChanged(object sender, EventArgs e)
+        private void AccelerationChanged(object sender, EventArgs e)
         {
             runLine.Acceleration = commonRunParameters.Acceleration;
             runTurn.Acceleration = commonRunParameters.Acceleration;
             runArc.Acceleration = commonRunParameters.Acceleration;
         }
 
-        void SpeedChanged(object sender, EventArgs e)
+        private void SpeedChanged(object sender, EventArgs e)
         {
             runLine.Speed = commonRunParameters.Speed;
             runTurn.Speed = commonRunParameters.Speed;
             runArc.Speed = commonRunParameters.Speed;
+        }
+
+        private void InitConsole()
+        {
+            rc = new RobotConsole();
+            console.RobotConsole = rc;
+            rc[Switches.Switch1].SwitchStateChanged +=
+                (_, __) => rc[Leds.Led1].LedEnabled = rc[Switches.Switch1].SwitchEnabled;
+            rc[Switches.Switch2].SwitchStateChanged +=
+                (_, __) => rc[Leds.Led2].LedEnabled = rc[Switches.Switch2].SwitchEnabled;
+            rc[Switches.Switch3].SwitchStateChanged +=
+                (_, __) => rc[Leds.Led3].LedEnabled = rc[Switches.Switch3].SwitchEnabled;
+            rc[Switches.Switch4].SwitchStateChanged +=
+                (_, __) => rc[Leds.Led4].LedEnabled = rc[Switches.Switch4].SwitchEnabled;
         }
     }
 }
